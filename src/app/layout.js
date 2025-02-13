48cb10d162360e2a6a0d916d138c78f0fd9fc3f8
Ali <aliuajibade317@gmail.com>
2025-02-13 17:46:57 +0100

    add favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="shortcut icon" href="favicon.ico" />
+        <link rel="shortcut icon" href="../../images/lacreteicon.png" />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
